8432dbdac58f5dae5c056a69245423ecde925786
Poenaru Petrut <96427905+TheRankN1@users.noreply.github.com>
2026-02-11 17:49:35 +0200

    feature(frontend) : add functions for socket service , for get messages , send message and for connections status + add environment

--- frontend/src/services/socket.service.ts
+++ frontend/src/services/socket.service.ts
@@ -1,9 +1,54 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy } from "@angular/core";
+import { io, Socket } from "socket.io-client";
+import { Observable } from "rxjs";
+import { ChatMessageInterface } from '../interfaces/chat-message.interface';
+import { environment } from '../environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
-export class SocketService {
+export class SocketService{
+  private socket!: Socket;
+  private readonly API_URL = environment.apiUrl;
 
-  constructor() { }
+  constructor() {
+    this.init();
+  }
+
+  public init(): void{
+    this.socket = io(this.API_URL, {
+      transports: ["websocket"],
+      autoConnect: true,
+    });
+  }
+
+  public onConnect(): Observable<string> {
+    return new Observable((subscriber) => {
+      const connFunction = () => subscriber.next(this.socket.id ?? "");
+      this.socket.on("connect", connFunction);
+
+      return () => this.socket.off("connect", connFunction);
+    });
+  }
+
+  public onConnectError(): Observable<any> {
+    return new Observable((subscriber) => {
+      const connErrFunction = (err: any) => subscriber.next(err);
+      this.socket.on("connect_error", connErrFunction);
+
+      return () => this.socket.off("connect_error", connErrFunction);
+    });
+  }
+
+  public onGetMessage(): Observable<ChatMessageInterface> {
+    return new Observable((subscriber) => {
+      const getMessage = (msg: ChatMessageInterface) => subscriber.next(msg);
+      this.socket.on("message", getMessage);
+      return () => this.socket.off("message", getMessage);
+    });
+  }
+
+  public sendMessage(message: ChatMessageInterface | string): void {
+    this.socket.emit("sendMessage", message);
+  }
 }
