ad28b54fdc47d6936b661bd0af29c966dd78f7a3
Poenaru Petrut <96427905+TheRankN1@users.noreply.github.com>
2026-02-11 17:50:55 +0200

    chore(frontend) : delete forgotten import for destroy

--- frontend/src/services/socket.service.ts
+++ frontend/src/services/socket.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, OnDestroy } from "@angular/core";
+import { Injectable } from "@angular/core";
 import { io, Socket } from "socket.io-client";
 import { Observable } from "rxjs";
 import { ChatMessageInterface } from '../interfaces/chat-message.interface';
