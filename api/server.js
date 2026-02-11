30a53539a7a2a57aec8057a4b66b829f3aeab8dd
Poenaru Petrut <96427905+TheRankN1@users.noreply.github.com>
2026-02-11 17:04:48 +0200

    feature(backend) : add socket server + sendMessage and disconnect events

--- api/server.js
+++ api/server.js
@@ -1,16 +1,37 @@
 import express from "express";
 import http from "http";
 import cors from "cors";
+import { Server as SocketIOServer } from "socket.io";
 import dotenv from 'dotenv';
-dotenv.config();
 
 const app = express();
 const server = http.createServer(app);
 const PORT = process.env.PORT
+const io = new SocketIOServer(server);
+
+dotenv.config();
 
 app.use(cors({
     origin: process.env.FRONTEND_URL,
 }));
+
+io.on("connection", (socket) => {
+    socket.on("sendMessage", (payload) => {
+        const text = payload?.text;
+
+        const message = {
+            text: text ?? "",
+            time: Date.now(),
+            senderId: socket.id,
+        };
+
+        io.emit("message", message);
+    });
+
+    socket.on("disconnect", () => {
+        console.log("An user has disconnected");
+    });
+});
 
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
